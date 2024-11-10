d0f466bcfe36613e5535f126ace727ccf358f889
Sreenidhi-G2 <152508973+Sreenidhi-G2@users.noreply.github.com>
2024-11-11 00:17:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+let typingEffect= new Typed("#text",{
+    strings:["Html","Css","JavaScript","Reactjs","Python","RestApi"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000
+})
+}
